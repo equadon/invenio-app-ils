94d289bf4d0d024747c2c8137adaa4c6829f3f0b
Niklas Persson <n.persson@cern.ch>
2020-01-13 10:08:09 +0100

    frontsite: add series details page
    * New URL endpoint for books: `/literature/abcde-12345`
    * New URL endpoint for series: `/series/abcde-12345`

---
+++ ui/src/pages/frontsite/components/Series/SeriesAbstract.js
@@ -0,0 +1,23 @@
+import ShowMore from 'react-show-more';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class SeriesAbstract extends Component {
+  render() {
+    const { metadata, lines } = this.props;
+    return (
+      <ShowMore
+        lines={lines}
+        more="Show more"
+        less="Show less"
+        anchorClass="button-show-more"
+      >
+        {metadata.abstract}
+      </ShowMore>
+    );
+  }
+}
+
+SeriesAbstract.propTypes = {
+  lines: PropTypes.number.isRequired,
+};
