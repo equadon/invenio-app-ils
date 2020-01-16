8349bfeac02541916ad91e88c74ae3f484f9bb6b
Niklas Persson <n.persson@cern.ch>
2020-01-16 17:24:17 +0100

    make series different in search results

--- ui/src/pages/frontsite/components/SeriesCard/SeriesCard.js
+++ ui/src/pages/frontsite/components/SeriesCard/SeriesCard.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image, Label } from 'semantic-ui-react';
+import { Card, Label, Image, Icon } from 'semantic-ui-react';
 import get from 'lodash/get';
-import { getCover } from '@pages/frontsite/config';
 import { goTo } from '@history';
 import { FrontSiteRoutes } from '@routes/urls';
 
@@ -19,28 +18,17 @@ export class SeriesCard extends Component {
           goTo(FrontSiteRoutes.seriesDetailsFor(data.metadata.pid))
         }
       >
-        <Card.Meta className={'discrete'}>{moi}</Card.Meta>
-        <Image
-          centered
-          src={getCover(data.metadata.edition)}
-          size={'small'}
-          onError={e => (e.target.style.display = 'none')}
-        />
+        <Card.Meta className={'discrete'}>
+          {moi}
+          <br />
+          <Icon size="huge" color="grey" name="list" />
+        </Card.Meta>
         <Card.Content>
           <Card.Header>{data.metadata.title}</Card.Header>
           <Card.Meta>
             <div>{data.metadata.authors.join(', ')}</div>
             <div>Edition {data.metadata.edition}</div>
           </Card.Meta>
-        </Card.Content>
-        <Card.Content extra>
-          <Label>
-            {moi === 'SERIAL'
-              ? `${get(data, 'metadata.relations.serial', []).length} issues`
-              : `${
-                  get(data, 'metadata.relations.multipart_monograph', []).length
-                } volumes`}
-          </Label>
         </Card.Content>
       </Card>
     );
