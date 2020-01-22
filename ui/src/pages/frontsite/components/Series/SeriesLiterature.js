d9dea5c615be829d6f95c5a275c38164dfa77994
Niklas Persson <n.persson@cern.ch>
2020-01-22 14:35:57 +0100

    series literature search first working example

--- ui/src/pages/frontsite/components/Series/SeriesLiterature.js
+++ ui/src/pages/frontsite/components/Series/SeriesLiterature.js
@@ -1,114 +1,192 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Divider, Item, Table } from 'semantic-ui-react';
+import { Divider, Table } from 'semantic-ui-react';
 import {
   ReactSearchKit,
   InvenioSearchApi,
+  // InvenioRequestSerializer,
   ResultsList,
   SearchBar,
 } from 'react-searchkit';
-import { document as documentApi } from '@api';
+import { literature as literatureApi } from '@api';
 import history from '@history';
+import Qs from 'qs';
+import _extend from 'lodash/extend';
+import isEmpty from 'lodash/isEmpty';
+import get from 'lodash/get';
+import { recordToPidType } from '@api/utils';
+import { SearchFooter } from '@components/SearchControls';
+import { Link } from 'react-router-dom';
+import { FrontSiteRoutes } from '@routes/urls';
 
-const SerialSearchBar = (_, executeSearch, onInputChange, queryString) => {
+const LiteratureSearchBar = (_, executeSearch, onInputChange, queryString) => {
   return (
     <SearchBar
       currentQueryString={queryString}
       onInputChange={onInputChange}
       executeSearch={executeSearch}
-      placeholder="Search books and series..."
+      placeholder="Search for volumes or issues in this series..."
     />
   );
 };
 
-const MultipartSearchBar = (_, executeSearch, onInputChange, queryString) => {
-  return (
-    <SearchBar
-      currentQueryString={queryString}
-      onInputChange={onInputChange}
-      executeSearch={executeSearch}
-      placeholder="Search books..."
-    />
+export class InvenioRequestSerializer {
+  constructor() {
+    this.serialize = this.serialize.bind(this);
+  }
+
+  _addFilter = (filter, filterUrlParams) => {
+    if (!Array.isArray(filter)) {
+      throw new Error(
+        `Filter value "${filter}" in query state must be an array.`
+      );
+    }
+    if (!(filter.length === 2 || filter.length === 3)) {
+      throw new Error(
+        `Filter value "${filter}" in query state must be an array of 2 or 3 elements`
+      );
+    }
+    const aggName = filter[0];
+    const fieldValue = filter[1];
+    filterUrlParams[aggName] = fieldValue;
+    const hasChild = filter.length === 3;
+    if (hasChild) {
+      this._addFilter(filter[2], filterUrlParams);
+    }
+  };
+
+  _addFilters = filters => {
+    if (!Array.isArray(filters)) {
+      throw new Error(`Filters query state "${filters}" must be an array.`);
+    }
+    /**
+     * input: [
+     *   [ 'type_agg', 'value1' ]
+     *   [ 'type_agg', 'value2', [ 'subtype_agg', 'a value' ] ]
+     * ]
+     */
+    const filterUrlParams = {};
+    filters.forEach(filter => {
+      this._addFilter(filter, filterUrlParams);
+    });
+    /**
+     * output: {
+     *  type_agg: 'value1'.
+     *  subtype_agg: 'a value'
+     * }
+     */
+    return filterUrlParams;
+  };
+
+  /**
+   * Return a serialized version of the app state `query` for the API backend.
+   * @param {object} stateQuery the `query` state to serialize
+   */
+  serialize(stateQuery) {
+    const { queryString, sortBy, sortOrder, page, size, filters } = stateQuery;
+
+    const getParams = {};
+    if (queryString !== null) {
+      getParams['q'] = queryString;
+    }
+    if (sortBy !== null) {
+      getParams['sort'] = sortBy;
+
+      if (sortOrder !== null) {
+        getParams['sort'] = sortOrder === 'desc' ? `-${sortBy}` : sortBy;
+      }
+    }
+    if (page > 0) {
+      getParams['page'] = page;
+    }
+    if (size > 0) {
+      getParams['size'] = size;
+    }
+    const filterParams = this._addFilters(filters);
+    _extend(getParams, filterParams);
+
+    return Qs.stringify(getParams, { arrayFormat: 'repeat' });
+  }
+}
+
+class LiteratureRequestSerializer extends InvenioRequestSerializer {
+  serialize(stateQuery) {
+    const relationQuery = 'relations.serial.pid:kezdn-xa684';
+    if (isEmpty(stateQuery.queryString)) {
+      stateQuery.queryString = relationQuery;
+    } else {
+      stateQuery.queryString = `${relationQuery} AND (${stateQuery.queryString})`;
+    }
+    const query = `${super.serialize(stateQuery)}&include_all`;
+    console.log('serialize', stateQuery, query);
+    return query;
+  }
+}
+
+const formatVolume = (result, parentPid) => {
+  const serials = get(result, 'metadata.relations.serial', []);
+  const parent = serials.find(
+    relation => relation.pid === parentPid && relation.pid_type === 'serid'
   );
+  return parent ? parent.volume : '?';
 };
 
-const SerialChildrenList = results => {
+const literatureResults = metadata => results => {
   return (
-    results && (
-      <Table basic>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>PID</Table.HeaderCell>
-            <Table.HeaderCell>Schema</Table.HeaderCell>
-            <Table.HeaderCell>Title</Table.HeaderCell>
-            <Table.HeaderCell>Document Type</Table.HeaderCell>
+    <Table basic>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Volume</Table.HeaderCell>
+          <Table.HeaderCell>Title</Table.HeaderCell>
+          <Table.HeaderCell>Type</Table.HeaderCell>
+          <Table.HeaderCell>Document Type / MOI</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {results.map(result => (
+          <Table.Row key={result.metadata.pid}>
+            <Table.Cell>{formatVolume(result, metadata.pid)}</Table.Cell>
+            <Table.Cell>
+              <Link
+                to={FrontSiteRoutes.documentDetailsFor(result.metadata.pid)}
+              >
+                {result.metadata.title}
+              </Link>
+            </Table.Cell>
+            <Table.Cell>
+              {recordToPidType(result) === 'docid' ? 'Document' : 'Series'}
+            </Table.Cell>
+            <Table.Cell>
+              {result.metadata.document_type ||
+                result.metadata.mode_of_issuance}
+            </Table.Cell>
           </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {results.map(result => (
-            <Table.Row key={result.metadata.pid}>
-              <Table.Cell>{result.metadata.pid}</Table.Cell>
-              <Table.Cell>{result.metadata.$schema}</Table.Cell>
-              <Table.Cell>{result.metadata.title}</Table.Cell>
-              <Table.Cell>{result.metadata.document_type}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    )
-  );
-};
-
-const MultipartChildrenList = results => {
-  return (
-    results && (
-      <Table basic>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>PID</Table.HeaderCell>
-            <Table.HeaderCell>Schema</Table.HeaderCell>
-            <Table.HeaderCell>Title</Table.HeaderCell>
-            <Table.HeaderCell>Document Type</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {results.map(result => (
-            <Table.Row key={result.metadata.pid}>
-              <Table.Cell>{result.metadata.pid}</Table.Cell>
-              <Table.Cell>{result.metadata.$schema}</Table.Cell>
-              <Table.Cell>{result.metadata.title}</Table.Cell>
-              <Table.Cell>{result.metadata.document_type}</Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-    )
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
 export class SeriesLiterature extends Component {
   get searchApi() {
     return new InvenioSearchApi({
-      url: documentApi.searchBaseURL,
+      invenio: {
+        requestSerializer: LiteratureRequestSerializer,
+      },
+      url: literatureApi.searchBaseURL,
       withCredentials: true,
     });
   }
 
   render() {
     const { metadata } = this.props;
-    const isSerial = metadata.mode_of_issuance === 'SERIAL';
     return (
       <>
         <Divider horizontal>Literature in this series</Divider>
         <ReactSearchKit searchApi={this.searchApi} history={history}>
-          <SearchBar
-            renderElement={isSerial ? SerialSearchBar : MultipartSearchBar}
-          />
-          <ResultsList
-            renderElement={
-              isSerial ? SerialChildrenList : MultipartChildrenList
-            }
-          />
+          <SearchBar renderElement={LiteratureSearchBar} />
+          <ResultsList renderElement={literatureResults(metadata)} />
+          <SearchFooter />
         </ReactSearchKit>
       </>
     );
